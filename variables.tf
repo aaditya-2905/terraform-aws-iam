910306e896221396b1b20b289e6820e7c11526a5
Aadityasinh zala <zalaaadityasinh9@gmail.com>
2026-04-02 15:05:14 +0530

    changes in the variable.tf

--- variables.tf
+++ variables.tf
@@ -4,13 +4,13 @@ variable "iam_config" {
   type = map(object({
     type = string
 
-    # ROLE CONFIG
+    # ROLE
     role_name          = optional(string)
     assume_role_policy = optional(any)
     role_description   = optional(string)
     role_path          = optional(string)
 
-    # USER CONFIG
+    # USER
     user_name               = optional(string)
     user_path               = optional(string)
     create_login_profile    = optional(bool)
@@ -18,24 +18,14 @@ variable "iam_config" {
     create_access_key       = optional(bool)
     access_key_status       = optional(string)
 
-    # POLICY CONFIG
+    # POLICY
     policy_name        = optional(string)
     policy_description = optional(string)
     policy_path        = optional(string)
     policy_json        = optional(any)
 
-    # COMMON CONFIG
+    # COMMON
     policy_arns = optional(list(string))
     tags        = optional(map(string))
   }))
-
-  default = {}
-
-  validation {
-    condition = alltrue([
-      for k, v in var.iam_config :
-      contains(["role", "user", "policy"], v.type)
-    ])
-    error_message = "Each iam_config object must have type = role, user, or policy."
-  }
 }
