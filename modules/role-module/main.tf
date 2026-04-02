35ea7e6d161b9ccab98ac68a1de991cd226761a6
Aadityasinh zala <zalaaadityasinh9@gmail.com>
2026-04-02 15:27:36 +0530

    changes in the variable.tf

--- modules/role-module/main.tf
+++ modules/role-module/main.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_role" "this" {
   name = var.role_name
 
-  assume_role_policy = can(tostring(var.assume_role_policy)) ? var.assume_role_policy : jsonencode(var.assume_role_policy)
+  assume_role_policy = try(tostring(var.assume_role_policy), jsonencode(var.assume_role_policy))
 
   description = var.role_description != null ? var.role_description : null
   path        = var.role_path != null ? var.role_path : "/"
